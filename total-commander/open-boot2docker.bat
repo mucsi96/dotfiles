e21c55b60235b8f84ee040ad7de483c5a64bc387
Igor Mucsicska <mucsi_96@yahoo.com>
2015-04-19 10:37:26 +0200

    fix boot2docker startup script

--- total-commander/open-boot2docker.bat
+++ total-commander/open-boot2docker.bat
@@ -3,6 +3,13 @@ setlocal ENABLEDELAYEDEXPANSION
 set lpath=/%1
 set lpath=%lpath:\=/%
 set lpath=%lpath::=%
+
 for /f %%i in ('boot2docker status') do set result=%%i
-if %result% neq "running" boot2docker up
+If NOT "%result%"=="%result:running=%" (
+    echo boot2docker is running
+) else (
+    echo boot2docker need to be started
+    boot2docker up
+)
+
 start "" "C:\Program Files (x86)\PuTTY\kitty.exe" -load "boot2docker" -cmd "cd %lpath% ; ./up.sh"
