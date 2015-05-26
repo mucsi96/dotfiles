9d279e2c6ca896467a087f17f5a662f7d63d45d1
Igor Mucsicska <mucsi_96@yahoo.com>
2015-05-26 20:29:48 +0200

    Fix boot2docker starter

--- total-commander/install.bat
+++
@@ -1,3 +0,0 @@
-copy %USERPROFILE%\AppData\Roaming\GHISLER\usercmd.ini %USERPROFILE%\AppData\Roaming\GHISLER\usercmd.ini.old
-copy usercmd.ini %USERPROFILE%\AppData\Roaming\GHISLER\usercmd.ini
-copy open-boot2docker.bat c:\totalcmd
