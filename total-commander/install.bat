b7b7ed26b326a1ff90bbf82dcd68480b8e5b18d8
Igor Mucsicska <mucsi_96@yahoo.com>
2015-05-30 16:50:01 +0200

    Vagrant-docker support added

---
+++ total-commander/install.bat
@@ -0,0 +1,4 @@
+cd /d %~dp0
+mklink "c:\totalcmd\open-boot2docker.bat" "%cd%\open-boot2docker.bat"
+mklink "c:\totalcmd\open-vagrant-docker.js" "%cd%\open-vagrant-docker.js"
+mklink "c:\totalcmd\usercmd.ini" "%cd%\usercmd.ini"
---
+++ total-commander/open-vagrant-docker.js
@@ -0,0 +1,37 @@
+var spawn = require('child_process').spawn,
+    path = formatPath(process.argv[2]);
+
+console.log('Docker directory: ' + path);
+
+function formatPath(path) {
+    path = path.replace(/\\/g, '/');
+    path = path.replace(/:/, '');
+    path = path.replace('c/Users/mucsi/gd/Dev', '/vagrant');
+    return path;
+}
+
+function sshIn() {
+    spawn('C:\\Program Files (x86)\\PuTTY\\kitty.exe', ['-load', 'vagrant', '-cmd', 'cd ' + path], {
+        detached: true,
+        stdio: 'inherit'
+    }).unref();
+}
+
+process.chdir('c:\\Users\\mucsi\\gd\\Dev\\docker');
+
+spawn('vagrant', ['status']).stdout.on('data', function(data) {
+    var output = new Buffer(data).toString('utf8')
+
+    if (output.indexOf('running') !== -1) {
+        console.log('Vagrant is running');
+        sshIn();
+    } else {
+        console.log('Vagrant need to be started');
+        spawn('vagrant', ['up'], {
+                stdio: ['ignore', 1, 2]
+            })
+            .on('exit', function() {
+                sshIn();
+            });
+    }
+});
