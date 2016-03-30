80b872d30bbabe02eca37e15d8bf36399fdcc393
Igor Mucsicska <mucsi_96@yahoo.com>
2016-03-30 10:51:17 +0200

    Improve vscode keybindings

--- vscode/install.js
+++ vscode/install.js
@@ -5,10 +5,20 @@ const os = require('os')
 const shell = require('shelljs')
 const pwd = __dirname
 let vscodeDest
+let keybindingsSrc
 
 switch (os.platform()) {
-    case 'darwin':
+  case 'linux':
+    vscodeDest = '~/.config/Code/User'
+    keybindingsSrc = `${pwd}/User/keybindings.linux.json`
+    break
+  case 'darwin':
     vscodeDest = '~/Library/Application\ Support/Code/User'
+    keybindingsSrc = `${pwd}/User/keybindings.osx.json`
+    break
+  case 'win32':
+    vscodeDest = '${process.env.USERPROFILE}/AppData/Roaming/Code/User'
+    keybindingsSrc = `${pwd}/User/keybindings.win.json`
     break
   default:
     shell.echo(`OS ${os.platform()} is currently not supported`)
@@ -18,5 +28,6 @@ switch (os.platform()) {
 if (vscodeDest && !shell.test('-e', vscodeDest)) {
   shell.echo('Installing Visual Studio Code config')
   shell.ln('-s', `${pwd}/User`, vscodeDest)
+  shell.ln('-s', keybindingsSrc, `${vscodeDest}/keybindings.json`)
 }
 
